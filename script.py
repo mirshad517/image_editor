217c7254391beb0592e295abcf91b72059df9919
mirshad517 <72601071+mirshad517@users.noreply.github.com>
2021-10-26 10:24:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,9 +2,8 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-I'm a Image Editor Bot which Supports various modes
-
-For more click help....</b>"""
+I'm Automatic Photo Editor എന്നെ develop ചെയ്തത് @dev_mirshad ആണ് എന്റെ ലാംഗ്വേജ് phython ആണ്
+എന്നിൽ നിന്നും എന്തെങ്കിലും error ഉണ്ടെങ്കിൽ എന്റെ മുതലാളിനെ @dev_mirshad നെ അറിയിക്കുക 😁😂....</b>"""
     HELP_MSG = """Hai, Follow these Steps..
 
 <code>🌀 Send me any Image to Edit..</code>
@@ -21,4 +20,4 @@ For more click help....</b>"""
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/mirshad517'>Click Here</a>"""
+⭕️<b>Source Code :</b> 👉 <a href='https://instagram.com/mirshad_kvr?utm_medium=copy_link>Click Here</a>"""

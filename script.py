daf722298f7b6dafcc8b641aec73e35377d897ec
mirshad517 <72601071+mirshad517@users.noreply.github.com>
2021-12-03 19:54:20 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,8 +2,16 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-I'm Automatic Photo Editor എന്നെ develop ചെയ്തത് @dev_mirshad ആണ് എന്റെ ലാംഗ്വേജ് phython ആണ്
-എന്നിൽ നിന്നും എന്തെങ്കിലും error ഉണ്ടെങ്കിൽ എന്റെ മുതലാളിനെ @dev_mirshad നെ അറിയിക്കുക 😁😂....</b>"""
+I'm Photo Editor Robot 🤖                           
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+@dev_mirshad....</b>"""
     HELP_MSG = """Hai, Follow these Steps..
 
 <code>🌀 Send me any Image to Edit..</code>
@@ -13,11 +21,11 @@ I'm Automatic Photo Editor എന്നെ develop ചെയ്തത് @dev_mi
 <code>🌀 Your Edited Image will be Uploaded </code>
 
 
-© @MIRSHAD """
-    ABOUT_MSG = """⭕️<b>My Name : Mirshad Auto Photo Editor</b>
+© @dev_mirshad """
+    ABOUT_MSG = """⭕️<b>My Name : Photo Editor Robot 🤖</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
 
-⭕️<b>Source Code :</b> 👉 <a href='https://instagram.com/mirshad_kvr?utm_medium=copy_link>Click Here</a>"""
+⭕️<b>Follow 😁 :</b> 👉 <a href='https://instagram.com/mirshad_kvr?utm_medium=copy_link>Click Here</a>"""
